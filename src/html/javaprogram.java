fec2a1d8f23c716e931defff7aadc813a8670808
manishapandu234 <106949245+manishapandu234@users.noreply.github.com>
2022-06-09 13:57:05 +0530

    Update javaprogram.java
    add another for loop

--- src/html/javaprogram.java
+++ src/html/javaprogram.java
@@ -5,8 +5,10 @@ public class javaprogram {
 	public static void main(String[] args) {
 		for(int i=1;i<=5;i++)
 		{
-			
-      System.out.println("manisha");
+			for(int j=1;j<=5;j++)
+			{
+      			System.out.println("manisha");
+			}
 	}
 	}
 }
